4e60a0a4ad6947436c81a0a96e21c8ff1dc7b6b7
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2026-09-07 01:08:31 +0200

    chore: simplify sample graph data to match the logo (a simple family of 4)

--- src/lib/sampleData.ts
+++ src/lib/sampleData.ts
@@ -1,66 +1,15 @@
-export const SAMPLE_FAMILY_TEXT = `Name, আক্কাস আলী
-Gender, Male
-Date of birth, 1935
-Date of death, 2012
-Wife, সালেহা বেগম
-Child, মতিউর রহমান
-Child, রোকসানা আক্তার
-Wife, খাদিজা খাতুন
-Child, সাজিদুর রহমান
+export const SAMPLE_FAMILY_TEXT = `নাম, আক্কাস আলী
+লিঙ্গ, পুরুষ
+স্ত্রী, সালেহা বেগম
+সন্তান, মতিউর রহমান
+সন্তান, রোকসানা আক্তার
 
-Name, সালেহা বেগম
-Gender, Female
-Date of death, 1985
+নাম, সালেহা বেগম
+লিঙ্গ, নারী
 
-Name, খাদিজা খাতুন
-Gender, Female
-Date of birth, 1950
+নাম, মতিউর রহমান
+লিঙ্গ, পুরুষ
 
-Name, মতিউর রহমান
-Gender, Male
-Date of birth, 1962
-Wife, নাজনীন আক্তার
-Child, নাদিম রহমান
-Child, ফারহানা রহমান
-
-Name, নাজনীন আক্তার
-Gender, Female
-Date of birth, 1968
-
-Name, নাদিম রহমান
-Gender, Male
-Date of birth, 1995
-
-Name, ফারহানা রহমান
-Gender, Female
-Date of birth, 1999
-
-Name, রোকসানা আক্তার
-Gender, Female
-Date of birth, 1966
-Husband, মোজাম্মেল হক
-Child, আবরার হক
-
-Name, মোজাম্মেল হক
-Gender, Male
-Date of birth, 1960
-
-Name, আবরার হক
-Gender, Male
-Date of birth, 1998
-
-Name, সাজিদুর রহমান
-Gender, Male
-Date of birth, 1978
-Wife, তানিয়া সুলতানা
-Child, আয়মান রহমান
-
-Name, তানিয়া সুলতানা
-Gender, Female
-Date of birth, 1984
-
-Name, আয়মান রহমান
-Gender, Male
-Date of birth, 2012
-`;
+নাম, রোকসানা আক্তার
+লিঙ্গ, নারী`;
 
